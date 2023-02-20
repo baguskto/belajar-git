d2706752705684ad317b83c1c2770b9f6064b3a8
Mohamad Fajar Hafidh <mfajarhafidh9@gmail.com>
2023-02-20 15:36:25 +0700

    feat: class comment fajar ln 32

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//Ini comment class fajar ha
+//Ini comment class fajar haf
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
